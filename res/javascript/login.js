c47cdc027fdd1e412119079841bc44a974d780e7
cjaq <81497746+cjaq@users.noreply.github.com>
2022-11-21 18:55:31 +0800

    Update login.js

--- res/javascript/login.js
+++ res/javascript/login.js
@@ -1,24 +1,36 @@
+function setMsg(msg){
+    $("#error_msg_wrapper").text(msg);
+    $("#error_msg_wrapper").css({
+        "opacity": "100%"
+    });
+    setTimeout(()=>{
+        $("#error_msg_wrapper").css({
+            "opacity": "0%"
+        });  
+    }, 1000);
+}
 window.onload = ()=>{
     $("#login").click(
         ()=>{
             let users = [{'account': 'root', 'password': '123', 'name': 'root'}];
             let account = $("input[name='account']").val();
             let password = $("input[name='password']").val();
-            for(let i = 0; i < users.length; ++i){
-                if(users[i].account == account && users[i].password == password){
-                    Cookies.set("login_user", users[i].name);
-                    $(location).attr('href', '/hdu-web-learn/form.html');
-                }else{
-                    $("#error_msg_wrapper").css({
-                        "opacity": "100%"
-                    });
-                    setTimeout(()=>{
-                        $("#error_msg_wrapper").css({
-                            "opacity": "0%"
-                        });  
-                    }, 1000);
-                }
+            if(account == ''){
+                setMsg("账号不能为空");
             }
+            else if(password == ''){
+                setMsg("密码不能为空");
+            }
+            else{
+                for(let i = 0; i < users.length; ++i){
+                    if(users[i].account == account && users[i].password == password){
+                        Cookies.set("login_user", users[i].name);
+                        $(location).attr('href', '/hdu-web-learn/form.html');
+                    }else{
+                        setMsg("账号或密码错误");
+                    }
+                }
+            }  
         }
     );
 }
