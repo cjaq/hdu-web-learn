52a207a153f1d9ddb1c663618aeff2ae9061798d
cjaq <81497746+cjaq@users.noreply.github.com>
2022-11-21 22:17:51 +0800

    Update header.js

--- res/javascript/header.js
+++ res/javascript/header.js
@@ -1,6 +1,6 @@
-$.get('/res/tpl/header.html', (result) => {
+$.get('/hdu-web-learn/res/tpl/header.html', (result) => {
     $("header").html(result);
     if(!Cookies.get("login_user")){
-        $("#logo").attr('src', '/res/images/3422.png_860.png');
+        $("#logo").attr('src', '/hdu-web-learn/res/images/3422.png_860.png');
     }
 });
