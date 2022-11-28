500040baa3dc2e207ad4467a79e9fce579d7716e
cjaq <81497746+cjaq@users.noreply.github.com>
2022-11-28 19:36:17 +0800

    Update add_item_in_main.js

--- res/javascript/add_item_in_main.js
+++ res/javascript/add_item_in_main.js
@@ -1,8 +1,22 @@
 /* 解析data数据 */
 /* data -> {'item' : [{'url': string, 'img': url, 'title': string, 'describe': string} ...]} */
 function create_html_objs_in_main(data){
+
+    //创建localstorage中用户新建的网页
+    let page_count = localStorage.getItem("page-count")
+    if(page_count != null){
+        page_count = parseInt(page_count);
+        data["用户自定义"] = [];
+        for(let i = 0; i < page_count; ++i){
+            data["用户自定义"].push({'url': './detail.html?res=localstorage&index=' + (i + 1).toString(), 
+            'title': localStorage.getItem("title" + (i + 1).toString()),
+            'img': 'res/images/logo.jpg' });
+        }
+    }
+
     let main_wrapper = document.createElement("div");
     main_wrapper.setAttribute("class", "content_wrapper");
+    //创建所有预定义的json文件对应的首页内容
     for(let key in data){
         let item_wrapper = document.createElement("div");
         let title = document.createElement("h1");
@@ -32,6 +46,7 @@ function create_html_objs_in_main(data){
         item_wrapper.appendChild(wrapper);
         main_wrapper.appendChild(item_wrapper);
     }
+    
     return main_wrapper;
 }
 
@@ -43,7 +58,6 @@ function load_content_from_server(){
     request.onload = () =>{
         if(request.status == 200){
             let data = JSON.parse(request.responseText);
-            console.log(data);
             document.getElementById("main_body").appendChild(create_html_objs_in_main(data));
         }else{
             alert("读取数据时出错");
@@ -51,4 +65,9 @@ function load_content_from_server(){
     }
 }
 
-load_content_from_server();
+window.onload = ()=>{
+    document.getElementById("add_icon").onclick = ()=>{
+        location.href = '/hdu-web-learn/form.html#/';
+    }
+    load_content_from_server();
+}
